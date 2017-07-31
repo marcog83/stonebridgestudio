2fee4ff57814912126b243fcad302a177b16d6ac
marcog83 <marcogobbi0@gmail.com>
2017-07-31 18:01:26 +0200

    first commit

--- static/js/main.js
+++ static/js/main.js
@@ -7,17 +7,18 @@ if (menu) {
     document.querySelector(".nav-trigger").addEventListener("click", function () {
         menu.classList.toggle("is-navopen");
     });
-    [].slice.call(document.querySelectorAll(".menu__item"), 0).forEach(function (btn) {
-        btn.addEventListener("click", function () {
-            menu.classList.remove("is-navopen");
-        })
-    })
+    // [].slice.call(document.querySelectorAll(".menu__item"), 0).forEach(function (btn) {
+    //     btn.addEventListener("click", function () {
+    //         menu.classList.remove("is-navopen");
+    //     })
+    // })
 }
 var posts = document.querySelector('.post-image-zoomed');
 if (posts) {
     posts.addEventListener("click", function (e) {
-
-        TweenMax.to(posts.querySelector("img"), .2, {
+        var textPanel = document.querySelector('.post-image-zoomed__text-panel');
+        TweenMax.to(textPanel,.3,{autoAlpha:0,y:100});
+        TweenMax.to(posts.querySelector("img"), .4, {
             autoAlpha: 0
         });
         TweenMax.to(posts, .5, {
@@ -34,6 +35,8 @@ if (posts) {
     [].slice.call(document.querySelectorAll(".post"), 0).forEach(function (post) {
         post.addEventListener("click", function () {
             var text = document.querySelector('.post-image-zoomed .text-js');
+            var textPanel = document.querySelector('.post-image-zoomed__text-panel');
+
             var scrollTop = 0//window.document.body.scrollTop;
             var originalThumb = post.querySelector("img");
             var textThumb = post.querySelector(".post__text p").textContent;
@@ -65,6 +68,7 @@ if (posts) {
                 width: isMobile ? "100%" : "auto",
                 height: isMobile ? "auto" : window.innerHeight - 54
             });
+            TweenMax.fromTo(textPanel,1,{autoAlpha:0,y:100},{autoAlpha:1,y:1});
         })
     })
 }
