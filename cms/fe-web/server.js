b4263e403125736e9a0b7514ba220b194ebc7596
marcog83 <marcogobbi0@gmail.com>
2017-08-05 12:27:05 +0200

    bozza cms

--- cms/fe-web/server.js
+++ cms/fe-web/server.js
@@ -25,14 +25,14 @@ app.use('/contents', contents);
 //
 const PORT = process.env.PORT || 5000;
 
-const options = {
-    key: fs.readFileSync('./fe-web/server.key'),
-    cert: fs.readFileSync('./fe-web/server.crt')
-};
+// const options = {
+//     key: fs.readFileSync('./fe-web/server.key'),
+//     cert: fs.readFileSync('./fe-web/server.crt')
+// };
 
-http2.createServer(options, app)
-    .listen(PORT, () => {
-            console.log(`Server is listening on https://localhost:${PORT}.
+// http2.createServer(options, app)
+app.listen(PORT, () => {
+            console.log(`Server is listening on http://localhost:${PORT}.
 You can open the URL in the browser.`)
         }
     );
